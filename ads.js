7d1904003804e4bcf0365ff219569903d6708f5b
yanjunbobo <yanjunbobo@users.noreply.github.com>
2026-05-09 16:55:04 +0800

    添加Google AdSense: ads.js并在所有页面引用

---
+++ ads.js
@@ -0,0 +1,8 @@
+// Google AdSense
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5382638197626690';
+    script.crossorigin = 'anonymous';
+    document.head.appendChild(script);
+})();
